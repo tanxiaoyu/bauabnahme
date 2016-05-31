db5073f44fe7e8e4cf84babe3d493ac81889a66e
specospec <vinuvnair@outlook.com>
2016-05-31 22:15:55 +0200

    changes for min max checking

--- HRSCTest.ino
+++ HRSCTest.ino
@@ -16,8 +16,8 @@
 #define trigPin 8 // Trigger Pin
 #define LEDPin 13 // Onboard LED
 
-int maximumRange = 200; // Maximum range needed
-int minimumRange = 0; // Minimum range needed
+int maximumRange = 350; // Maximum range needed
+int minimumRange = 2; // Minimum range needed
 long duration, distance; // Duration used to calculate distance
 
 void setup() {
@@ -41,19 +41,19 @@ void loop() {
  
  //Calculate the distance (in cm) based on the speed of sound.
  distance = duration/58.2;
-  Serial.println(distance);
-// if (distance >= maximumRange || distance <= minimumRange){
+  
+ if (distance >= maximumRange || distance <= minimumRange){
 // /* Send a negative number to computer and Turn LED ON 
 // to indicate "out of range" */
-// Serial.println("-1");
-// digitalWrite(LEDPin, HIGH); 
-// }
-// else {
-// /* Send the distance to the computer using Serial protocol, and
+ Serial.println("-1");
+ digitalWrite(LEDPin, HIGH); 
+ }
+else {
+ /* Send the distance to the computer using Serial protocol, and
 // turn LED OFF to indicate successful reading. */
-// Serial.println(distance);
-// digitalWrite(LEDPin, LOW); 
-// }
+ Serial.println(distance);
+ digitalWrite(LEDPin, LOW); 
+ }
  
  //Delay 50ms before next reading.
  delay(50);
